026f6cec0548c8aad0ff24f403bdc0a73cdc3a27
Princewill Jeremiah <princewilljeremiah83@gmail.com>
2024-09-13 17:17:47 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
-import heroImg from './components/img/image.png'
+import heroImg from './components/img/pexels-shkrabaanthony-6759181.jpg'
 
 export default function Home() {
   return (
     <div>
       <div className="z-50 fixed left-1/2 transform -translate-x-1/2 w-full inset-0 flex items-center justify-center bg-black bg-opacity-50">
-        <div className="relative left-[-40px]  md:right-[10%] text-6xl md:text-7xl font-bold w-[10%] m-auto">
+        <div className="relative left-[-120px] -top-[60px]  md:right-[10%] text-6xl md:text-[95px] font-bold w-[10%] m-auto">
           <div className="absolute w-full text-center animate-slide-in-right-smooth text-stroke-[1px] text-stroke-[#ffffff96] text-transparent">
             COMING SOON!!
           </div>
@@ -21,7 +21,7 @@ export default function Home() {
         </div>
       </div>
 
-      <section className="blur-sm">
+      <section className="blur-md">
         <div className="md:flex flex-row-reverse justify-between items-center overflow-hidden max-h-[100vh]">
           <div className="hero-img">
             <Image src={heroImg} alt="tkmotive hero" className="h-[500px] md:h-[100%] md:w-[740px] object-fit" />
